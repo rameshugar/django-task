7ba10bda24bae479676a69e75289151ef1706d5b
manjunath <manjunath@agiliq.com>
2016-06-21 11:55:36 +0530

    api to get word count

---
+++ my_app/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+
+import json
+from collections import OrderedDict
+
+# Create your views here.
+
+@csrf_exempt
+def home(request):
+    x = json.loads(request.body)
+    y = x.get('string')
+    to_list = y.split()
+    new_list = [i.strip(',.') for i in to_list]
+    result = OrderedDict()
+    for i in new_list:
+        result[i] = new_list.count(i)
+    return HttpResponse(json.dumps(result), content_type="application/json")
