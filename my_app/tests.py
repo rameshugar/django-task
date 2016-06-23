bd7faa69362b75ff3f7642fc3cada05c82a14080
manjunath <manjunath@agiliq.com>
2016-06-23 12:52:48 +0530

    wrote test case

--- my_app/tests.py
+++ my_app/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.test.client import Client
+import json
 
-# Create your tests here.
+class TestCase(TestCase):
+    def setUp(self):
+        self.c = Client()
+
+    def test_api(self):
+        string = {'string': 'hi, hi, hello'}
+        string = json.dumps(string)
+        response = self.c.post('/api/', content_type='application/json', data=string)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"hi":2, "hello":1})
